86fc2f6316f6fd06b972960466477d131f55412f
FatemehAllameh <fatemeh.allameh19@gmail.com>
2026-01-01 23:00:18 +0330

    Add weather API fetch logic

---
+++ script.js
@@ -0,0 +1,21 @@
+// Elements
+const searchForm = document.querySelector(".search-form");
+const searchInput = document.querySelector(".search-input");
+
+// API Data
+const API_KEY = "8198d3e30961c9df4165a740202792c3";
+const API_URL = `https://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}&q=`;
+
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (searchInput.value.trim()) {
+    getWeather(API_URL + searchInput.value);
+  }
+});
+
+const getWeather = async (url) => {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+};
+
