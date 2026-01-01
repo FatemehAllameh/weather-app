cf443fa645d4ae927337bc00972fd68447ace27f
FatemehAllameh <fatemeh.allameh19@gmail.com>
2026-01-01 23:02:49 +0330

    Handle city not found error

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Elements
 const searchForm = document.querySelector(".search-form");
 const searchInput = document.querySelector(".search-input");
+const errorMessage = document.querySelector(".error-message");
 
 // API Data
 const API_KEY = "8198d3e30961c9df4165a740202792c3";
@@ -16,6 +17,13 @@ searchForm.addEventListener("submit", (e) => {
 const getWeather = async (url) => {
   const response = await fetch(url);
   const data = await response.json();
+  if (data.cod === "404") {
+    displayErroe(data.message);
+  }
   console.log(data);
 };
 
+const displayErroe = (error) => {
+  errorMessage.style.display = "block";
+  errorMessage.textContent = error;
+};
